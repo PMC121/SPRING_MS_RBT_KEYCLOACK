85879e47bb87cc0b6f15322231d060b6ba278d81
PMC121 <prashantchavan712@gmail.com>
2021-08-15 17:43:50 +0530

    first commit

---
+++ CongurationServer/src/main/java/com/appsdevlopers/main/CongurationServerApplication.java
@@ -0,0 +1,13 @@
+package com.appsdevlopers.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongurationServerApplication.class, args);
+	}
+
+}
---
+++ CongurationServer/src/test/java/com/appsdevlopers/main/CongurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdevlopers.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
